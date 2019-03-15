075e3f2dccb45ff740b4e1a95bcc0cdc077fbd78
adrien <adrien.basile80@gmail.com>
2019-03-15 16:09:08 +0100

    mysqlmeteomanger.cpp

--- cpp/adrien/MysqlManager/MysqlMeteoManager.cpp
+++ cpp/adrien/MysqlManager/MysqlMeteoManager.cpp
@@ -2,24 +2,6 @@
 
 #pragma hdrstop
 
-#include "MysqlMeteoManager.h"
-#include "../classeBDD/Bdd.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 
-
-class MysqlMeteoManager {
-   private:
-     
-      static BDD* bdd;
-      static MysqlMeteoManager *m_instance;  
-      MysqlMeteoManager(); 
-      ~MysqlMeteoManager();
-
-   public:
-   
-       MysqlMeteoManager * getInstance();
-       bool connect();
-       bool InsertDonnéeCapteur();  
-
-}
--- cpp/adrien/MysqlManager/MysqlMeteoManager.h
+++ cpp/adrien/MysqlManager/MysqlMeteoManager.h
@@ -2,5 +2,24 @@
 
 #ifndef MysqlManagerH
 #define MysqlManagerH
+#include "MysqlMeteoManager.h"
+#include "../classeBDD/Bdd.h"
 //---------------------------------------------------------------------------
 #endif
+
+
+class MysqlMeteoManager {
+   private:
+     
+      static BDD* bdd;
+      static MysqlMeteoManager *m_instance;  
+      MysqlMeteoManager(); 
+      ~MysqlMeteoManager();
+
+   public:
+   
+       MysqlMeteoManager * getInstance();
+       bool connect();
+       bool InsertDonnéeCapteur();  
+
+}

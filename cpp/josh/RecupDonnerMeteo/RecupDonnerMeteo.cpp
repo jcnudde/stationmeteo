e83f93f144936a87b93ef8697f535b4d657738a1
jcnudde <jcnudde80@gmail.com>
2019-03-16 11:33:25 +0100

    -class recupDonner manque la classe sqlManager pour envoyer les données Meteo version 1.0

--- cpp/josh/RecupDonnerMeteo/RecupDonnerMeteo.cpp
+++ cpp/josh/RecupDonnerMeteo/RecupDonnerMeteo.cpp
@@ -1,28 +1,64 @@
 #include "RecupDonnerMeteo.h"
 
-RecupDonner::RecupDonner()
+RecupDonnerMeteo::RecupDonnerMeteo()
 {
     capteur.anemometre = new Anemometre(0);
-    capteur.thermometre = new Girouette(1);
+    capteur.girouette = new Girouette(1);
     capteur.barometre = new Barometre(2);
-    capteur.thermometre = NULL;
-    capteur.hygrometre = NULL;
-    capteur.luxmetre = NULL;
+    capteur.thermometre = new Thermometre(); //  manque le constructeur
+    capteur.hygrometre = new Hygrometre(); //  manque le constructeur
+    capteur.solarimetre = new Solarimetre(); //  manque le constructeur
     capteur.capteur_JourNuit = new CapteurJour_Nuit(8);
     capteur.capteurPluie = new CapteurPluie(7);
-    capteur.pluviometre = new Pluiviometre(9);       
-}
+    capteur.pluviometre = new Pluiviometre(9); 
 
-RecupDonner::~RecupDonner()
+    Thread=CreateThread(
+	NULL,
+	0,
+	MonThread,
+	this,
+	0,
+	&dwThreadIdArray); 
+}
+RecupDonnerMeteo::~RecupDonnerMeteo()
 {
     //dtor
 }
-tabDonnerCapteur RecupDonner::getDonner()
+DWORD WINAPI RecupDonnerMeteo::ThreadRecupDonnee(LPVOID params)
+{
+    RecupDonnerMeteo * recupDonnerMeteo = (RecupDonnerMeteo*) params;
+
+    while(true)
+    {
+        tabDonnerCapteur donneeMeteoThread;
+
+        donneeMeteoThread.vitesseVent = recupDonnerMeteo->capteur.anemometre->readValue();
+        donneeMeteoThread.direction = recupDonnerMeteo->capteur.girouette->readValue();
+        donneeMeteoThread.pressionAtmospherique = recupDonnerMeteo->capteur.barometre->readValue();
+        donneeMeteoThread.direction = recupDonnerMeteo->capteur.girouette->readValue();
+        donneeMeteoThread.hummiditeRelative = recupDonnerMeteo->capteur.hygrometre->readValue();
+        donneeMeteoThread.luminosite = recupDonnerMeteo->capteur.solarimetre->readValue();
+        donneeMeteoThread.jour = recupDonnerMeteo->capteur.capteur_JourNuit->readValue();
+        donneeMeteoThread.pluie = recupDonnerMeteo->capteur.capteurPluie->readValue();
+        donneeMeteoThread.surfaceDePluie = recupDonnerMeteo->capteur.pluviometre->readValue(); 
+
+        //attentte classe sqlMeteoManager pour insert les donnée meteo   
+    }
+
+}
+tabDonnerCapteur RecupDonnerMeteo::getDonner()
 {
     tabDonnerCapteur donneeMeteo;
 
-    donneMeteo.vitesseVent=capteur.anemometre->readValue();
+    donneeMeteo.vitesseVent=capteur.anemometre->readValue();
+    donneeMeteo.direction = capteur.girouette->readValue();
+    donneeMeteo.pressionAtmospherique=capteur.barometre->readValue();
+    donneeMeteo.direction=capteur.girouette->readValue();
+    donneeMeteo.hummiditeRelative=capteur.hygrometre->readValue();
+    donneeMeteo.luminosite=capteur.solarimetre->readValue();
+    donneeMeteo.jour=capteur.capteur_JourNuit->readValue();
+    donneeMeteo.pluie=capteur.capteurPluie->readValue();
+    donneeMeteo.surfaceDePluie=capteur.pluviometre->readValue();
 
-    
-    
+    return donneeMeteo;
 }
--- cpp/josh/RecupDonnerMeteo/RecupDonnerMeteo.h
+++ cpp/josh/RecupDonnerMeteo/RecupDonnerMeteo.h
@@ -1,8 +1,9 @@
 /**
- * RecupDonner
+ * Fichier : RecupDonner.H
  * 
  * Cette classe permet de utiliser les donner des capteur,les gardes
- * en mémoire, les insert dans la base de donnée
+ * en mémoire, les insert dans la base de donnée. Elle appuyer sur le 
+ * principe du singleton
  * 
  * Copyright 7 mars 2019 (@author CNUDDE Joshua)
  * @version 1.0
@@ -16,13 +17,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <map>
+#include <windows.h>
 #include "../Capteur/Anemometre.h"
 #include "../Capteur/Girouette.h"
 #include "../Capteur/Barometre.h"
 #include "../../adrien/Capteur/CapteurJour_Nuit.h"
 #include "../../adrien/Capteur/CapteurPluie.h"
 #include "../../adrien/Capteur/Pluiviometre.h"
+#include "../../louis/Capteur/Hygrometre.h"
+#include "../../louis/Capteur/Solarimetre.h"
+#include "../../louis/Capteur/Thermometre.h"
 
 
 using namespace std;
@@ -34,7 +38,7 @@ typedef struct tabCapteur
     Capteur *barometre;
     Capteur *thermometre;
     Capteur *hygrometre;
-    Capteur *luxmetre;
+    Capteur *solarimetre;
     Capteur *capteur_JourNuit;
     Capteur *capteurPluie;
     Capteur *pluviometre;
@@ -56,20 +60,21 @@ class RecupDonnerMeteo
     private:
         SqlMeteoManager* sqlMeteo;
         tabCapteur capteur;
-        static ThreadRecupDonnee(RecupDonner*);
+        HANDLE Thread;
+        DWORD dwThreadIdArray;
         //constructeur
-        RecupDonner();
+        RecupDonnerMeteo();
         //destructeur
-        ~RecupDonner();
+        ~RecupDonnerMeteo();
+
+        static DWORD WINAPI ThreadRecupDonnee(LPVOID params);
 
     public:
-        
+        //renvoie les données météo aquise à l'instant t sous forme d'une structure
         tabDonnerCapteur getDonner();
+        //renvoie un seul et même pointeur
         RecupDonner* getInstance();
 
-    
-
-   
 };
 
-#endif // RECUPDONNER_H
+#endif // RECUPDONNERMETEO_H
